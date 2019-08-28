2cbfb532d214d3d8efa700d58e6f16e761e47083
xJ4111 <e3arul@gmail.com>
2019-08-28 23:12:10 +0100

    Enemy Platformer Pathfinding

--- The Unreliable Narrator/Assets/Scripts/EnemyScript.cs
+++ The Unreliable Narrator/Assets/Scripts/EnemyScript.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    //Stats
+    public int HP = 100;
+
+    //Physics
+    private float speed = 0.05f;
+    private float detectRange = 9f;
+    private float jumpSpeed;
+    private float jumpForce = 6f;
+    private bool isGrounded = true;
+
+    //Raycast
     public LayerMask layersToHit;
+    private Rigidbody2D rigid;
+    RaycastHit2D left, right, down;
+    public Transform feetPosition;
 
-    int hp = 100;
-    Rigidbody2D rigid;
-    PlayerMovementController player;
+    //Pathfinding
+    private PlayerMovementController player;
+    float disToPlayer;
+    private bool playerFound;
+    private int playerDir;
+    private Vector2 playerLastPos;
+    private Vector2 v2null = new Vector2(-12345, -12345);
 
-    public Vector2 lastKnownPos;
-    bool playerFound;
+    public bool MoveType = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         player = GameObject.Find("PlayerControllerTopDown").GetComponent<PlayerMovementController>();
+        jumpSpeed = speed / 2;
+        playerLastPos = v2null;
     }
 
     // Update is called once per frame
@@ -25,32 +44,99 @@ public class EnemyScript : MonoBehaviour
     {
         FindPlayer();
         ChasePlayer();
+        AttackPlayer();
+
+        disToPlayer = Vector2.Distance(transform.position, player.transform.position);
     }
 
     public void TakeDamage(int damage)
     {
-        hp -= damage;
+        HP -= damage;
+        KnockBack();
 
-        if (hp <= 0)
+        if (HP <= 0)
             Destroy(gameObject);
     }
 
+    void AttackPlayer()
+    {
+        if(disToPlayer < 1f)
+        {
+            //player.hp--;
+            PlayerKnockBack();
+        }
+    }
+
+    void KnockBack()
+    {
+        if (playerDir == 1)
+            rigid.AddForce(new Vector2(100f,100f));
+        else if(playerDir == -1)
+            rigid.AddForce(new Vector2(-100f, 100f));
+    }
+
+    void PlayerKnockBack()
+    {
+        if (playerDir == 1)
+            player.rb.AddForce(new Vector2(-100f, 100f));
+        else if (playerDir == -1)
+            player.rb.AddForce(new Vector2(100f, 100f));
+    }
+
+    #region Pathfinding
     void FindPlayer()
     {
-        Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, 10000000f, layersToHit);
-        Debug.Log(hit.transform.name);
+        if (transform.position.x < player.transform.position.x)
+            playerDir = 1;
+        if(transform.position.x > player.transform.position.x)
+            playerDir = -1;
 
-        if (hit.transform.tag == "Player")
-        {
-            playerFound = true;
-            lastKnownPos = hit.transform.position;
-        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, 100f, layersToHit);
+        playerFound = (hit.transform.tag == player.transform.tag && disToPlayer <= detectRange);
+
+        if (playerFound)
+            playerLastPos = player.transform.position;
     }
 
     void ChasePlayer()
     {
-        if (playerFound && Vector2.Distance(transform.position, player.transform.position) > 1.6f)
-            transform.Translate((lastKnownPos - (Vector2)transform.position) * 1f * Time.deltaTime);
+        if (Vector2.Distance(transform.position, playerLastPos) > 1f)
+        {
+            Move();
+            JumpCheck();
+        }
     }
+
+    void Move()
+    {
+        if (disToPlayer > 1f && playerLastPos != v2null)
+        {
+            if(isGrounded)
+                transform.position = new Vector2(Vector2.MoveTowards(transform.position, playerLastPos, speed).x, transform.position.y);
+            else
+                transform.position = new Vector2(Vector2.MoveTowards(transform.position, playerLastPos, jumpSpeed).x, transform.position.y);
+        }
+    }
+
+    void JumpCheck()
+    {
+        GroundedCheck();
+
+        right = Physics2D.Raycast(transform.position, transform.right, 1, layersToHit);
+        left = Physics2D.Raycast(transform.position, -transform.right, 1, layersToHit);
+
+        if (isGrounded && ((right && right.transform.tag != player.transform.tag) || (left && left.transform.tag != player.transform.tag)))
+        {
+            rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void GroundedCheck()
+    {
+        down = Physics2D.Raycast(feetPosition.position, Vector2.down, 0.05f, player.FloorLayermask);
+        isGrounded = down.collider != null;
+    }
+
+    #endregion
 }
