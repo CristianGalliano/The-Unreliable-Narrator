74db00b0cae4d31f9d7a0c110849a4b063a40dab
Graithen <grey.jim@gmail.com>
2019-08-31 19:01:50 +0100

    End State controller script

---
+++ The Unreliable Narrator/Assets/AssetEndState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetEndState : MonoBehaviour
+{
+    public bool EndState = false;
+    public GameObject[] Assets;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EndState)
+        {
+            Assets[0].SetActive(false);
+            Assets[1].SetActive(true);
+        }
+    }
+}
